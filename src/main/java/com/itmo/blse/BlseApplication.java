6fe321022302caf611a4ef4f52775360cc4ce0dc
Artem Dybov <artem.dybov@webium.team>
2023-03-20 20:04:38 +0300

    Initial commit

---
+++ src/main/java/com/itmo/blse/BlseApplication.java
@@ -0,0 +1,13 @@
+package com.itmo.blse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlseApplication.class, args);
+    }
+
+}
