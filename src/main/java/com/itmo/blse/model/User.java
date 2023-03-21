5347ed7f69a08598bf7174398af086b02a64ab5d
Artem Dybov <artem.dybov@webium.team>
2023-03-21 21:03:16 +0300

    abuba

--- src/main/java/com/itmo/blse/model/User.java
+++ src/main/java/com/itmo/blse/model/User.java
@@ -1,6 +1,13 @@
 package com.itmo.blse.model;
 
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table
 public class User {
+    @Id
     Long id;
+    @Column(nullable = false)
     String username;
 }
